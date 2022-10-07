fef64fd193cc746be27588db1dc20c2c99adb9b4
Nikita <nikita.shkuratt@gmail.com>
2022-10-07 23:14:12 +0300

    aaditional classwork

---
+++ practice/generator.js
@@ -0,0 +1,33 @@
+//generator exmaple
+const country = {
+    values: ['ua', 'uk', 'pl', 'usa'],
+    [Symbol.iterator]() {
+        let i = 0
+        return {
+            next: () => {
+                const value = this.values[i]
+                i++
+                return {
+                    done: i > this.values.length,
+                    value
+                }
+            }
+        }
+    }
+}
+
+for (let item of country) {
+    console.log(item)
+}
+
+// iteration generator
+function* gen(num) {
+    for (let i = 0; i < num; i++) {
+        yield i
+    }
+}
+
+// const iter = gen(3)
+for (let i of gen(5)) {
+    console.log(i)
+}
