300c917dab07e6c3dca3858b47c7a5ad324405b6
Nikita <nikita.shkuratt@gmail.com>
2023-01-21 21:07:54 +0200

    corrected

--- lesson17-18/fetch.js
+++ lesson17-18/fetch.js
@@ -3,28 +3,90 @@ let options =
 {
    method: 'GET',
 }
-
+const containerData = document.querySelector('.wrapperData')
 let req = fetch(url, options);
-req
-.then(response => response.json())
-.then(async data => {
-   console.log(data.results[0]);
+
+function showPlanets(data){
+   document.querySelector(".loader").classList.add("active")
+
+   let outputPlanets = []
    for (let i = 0; i < data.results.length; i++) {
       let dtRes = data.results[i]
-
       let outputPlanet = `
-         <ul data-name-planet="${dtRes['name']}">Planet name: ${dtRes['name']}, Terrain: ${dtRes['terrain']}, Climat: ${dtRes['climate']}
-         </ul>
-         <hr>
-      `
-      document.body.insertAdjacentHTML('beforeend', outputPlanet)
-      for (const el of dtRes.residents) {
-          fetch(el, options).then(data_json => data_json.json()).then(data => {
-          // await fetch(el, options).then(data_json => data_json.json()).then(data => {
-            document.querySelector(`[data-name-planet="${dtRes['name']}"]`)
-                .insertAdjacentHTML('beforeend', `<li>${data.name}</li>`)
+      <ul class="planets" data-residents="${dtRes['residents']}" data-name-planet="${dtRes['name']}">Planet name: ${dtRes['name']}, Terrain: ${dtRes['terrain']}, Climat: ${dtRes['climate']}
+      </ul>
+      <hr>
+   `
+      outputPlanets.push(outputPlanet)
+   }
+      containerData.innerHTML = outputPlanets.join('')
+      let planets = document.querySelectorAll('.planets')
+      planets.forEach(e => {
+         e.onclick = showResidents
+      })
+   document.querySelector(".loader").classList.remove("active")
+
+}
+
+function showResidents() {
+
+   let residents = this.dataset.residents.split(',')
+   if (!this.children.length) {
+      document.querySelector(".loader").classList.add("active")
+
+      for (const el of residents) {
+         fetch(el, options).then(data_json => data_json.json()).then(data => {
+            this.insertAdjacentHTML('beforeend', `<li>${data.name}</li>`)
          });
       }
+
+      document.querySelector(".loader").classList.remove("active")
+
+   }else{
+      [...this.children].forEach(e => e.remove())
    }
+}
+
+
+
+function showNbu(data){
+   console.log(data);
+   let outputDataExc = []
+   output = `<table>
+  <tr>
+    <th>Валюта</th>
+    <th>Курс</th>
+    <th>СС</th>
+    <th>Дата</th>
+  </tr>
+  ${data.map(el => {
+      return `
+      <tr>
+          <td>${el.txt}</td>
+          <td>${el.rate}</td>
+          <td>${el.cc}</td>
+          <td>${el.exchangedate}</td>
+       </tr>
+      `
+   }).join('')}
+  
+</table>`
+   containerData.innerHTML = output
+}
+
+req.then(response => response.json())
+    .then(data => starWars.addEventListener('click', function (){
+       showPlanets(data)
+    }))
+    .catch(err => {console.error(err);  });
+
+nbu.addEventListener('click', function (){
+   document.querySelector(".loader").classList.add("active")
+   fetch('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json', options)
+       .then(data => data.json())
+       .then(data => {
+      showNbu(data)
+   })
+   document.querySelector(".loader").classList.remove("active")
+
 })
-.catch(err => {console.error(err);  });
--- lesson17-18/promise.js
+++ lesson17-18/promise.js
@@ -1,4 +1,4 @@
-async function userInfo(url) {
+function userInfo(url) {
 
     return new Promise((resolve, reject) => {
 
@@ -22,25 +22,95 @@ async function userInfo(url) {
     });
 }
 
-userInfo("https://swapi.dev/api/planets/")
-    .then(data => {
-        console.log(data);
-        for (let i = 0; i < data.results.length; i++) {
-            let dtRes = data.results[i]
 
-            let outputPlanet = `
-         <ul data-name-planet="${dtRes['name']}">Planet name: ${dtRes['name']}, Terrain: ${dtRes['terrain']}, Climat: ${dtRes['climate']}
-         </ul>
-         <hr>
-      `
-            document.body.insertAdjacentHTML('beforeend', outputPlanet)
-            for (const el of dtRes.residents) {
-                userInfo(el).then(data_json => data_json).then(data => {
-                    // await fetch(el, options).then(data_json => data_json.json()).then(data => {
-                    document.querySelector(`[data-name-planet="${dtRes['name']}"]`)
-                        .insertAdjacentHTML('beforeend', `<li>${data.name}</li>`)
-                });
-            }
-        }
+
+let url = 'https://swapi.dev/api/planets/';
+let options =
+    {
+        method: 'GET',
     }
-    );
+const containerData = document.querySelector('.wrapperData')
+let req = userInfo(url);
+
+function showPlanets(data){
+    document.querySelector(".loader").classList.add("active")
+
+    let outputPlanets = []
+    for (let i = 0; i < data.results.length; i++) {
+        let dtRes = data.results[i]
+        let outputPlanet = `
+      <ul class="planets" data-residents="${dtRes['residents']}" data-name-planet="${dtRes['name']}">Planet name: ${dtRes['name']}, Terrain: ${dtRes['terrain']}, Climat: ${dtRes['climate']}
+      </ul>
+      <hr>
+   `
+        outputPlanets.push(outputPlanet)
+    }
+    containerData.innerHTML = outputPlanets.join('')
+    let planets = document.querySelectorAll('.planets')
+    planets.forEach(e => {
+        e.onclick = showResidents
+    })
+    document.querySelector(".loader").classList.remove("active")
+
+}
+
+function showResidents() {
+
+    let residents = this.dataset.residents.split(',')
+    if (!this.children.length) {
+        document.querySelector(".loader").classList.add("active")
+
+        for (const el of residents) {
+            userInfo(el).then(data => {
+                this.insertAdjacentHTML('beforeend', `<li>${data.name}</li>`)
+            });
+        }
+
+        document.querySelector(".loader").classList.remove("active")
+
+    }else{
+        [...this.children].forEach(e => e.remove())
+    }
+}
+
+
+function showNbu(data){
+    console.log(data);
+    let outputDataExc = []
+    output = `<table>
+  <tr>
+    <th>Валюта</th>
+    <th>Курс</th>
+    <th>СС</th>
+    <th>Дата</th>
+  </tr>
+  ${data.map(el => {
+        return `
+      <tr>
+          <td>${el.txt}</td>
+          <td>${el.rate}</td>
+          <td>${el.cc}</td>
+          <td>${el.exchangedate}</td>
+       </tr>
+      `
+    }).join('')}
+  
+</table>`
+    containerData.innerHTML = output
+}
+
+
+req.then(data => starWars.addEventListener('click', function (){
+        showPlanets(data)
+    }))
+    .catch(err => {console.error(err);  });
+
+nbu.addEventListener('click', function (){
+    document.querySelector(".loader").classList.add("active")
+    userInfo('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json')
+        .then(data => {
+            showNbu(data)
+        })
+    document.querySelector(".loader").classList.remove("active")
+
+})
